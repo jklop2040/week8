318f39a01c5e18b32121f5f596b39166f32c6321
Jake Klopfenstein <jake.klopfenstein@kellogg.northwestern.edu>
2021-02-25 19:31:17 -0600

    Firestore add

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyCkdYmZlPbEh5ZzDku_9nHYDZOTV4Qt2J8",
+  authDomain: "kiei-451-76acb.firebaseapp.com",
+  projectId: "kiei-451-76acb",
+  storageBucket: "kiei-451-76acb.appspot.com",
+  messagingSenderId: "25260936786",
+  appId: "1:25260936786:web:48082db9aa53f41cc4198b"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
